a370663a95c151d03394d3c541065d627931d914
oladojabasit29@gmail.com <oladojabasit29@gmail.com>
2026-03-07 17:04:12 +0100

    updated axios

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://jobapplytracker-v2.onrender.com",
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
 });
 
