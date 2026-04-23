b34c872c7fce66c77f5d7eac5b665bbc5251bf9b
oladojabasit29@gmail.com <oladojabasit29@gmail.com>
2026-04-23 11:28:38 +0100

    Server Updated

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,13 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-import mongoose from 'mongoose';
 import jobRoutes from './routes/jobRoutes.js';
 import authRoutes from './routes/authRoutes.js';
 import { errorHandler } from './middleware/errorHandler.js';
 
 const app = express();
 dotenv.config();
+app.set('trust proxy', 1);
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
@@ -16,10 +16,6 @@ app.use(cors({
     credentials: true,
 }));
 const PORT = process.env.PORT || 5000;
-
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('Could not connect to MongoDB', err));
 
 app.use("/jobs", jobRoutes);
 app.use("/auth", authRoutes);
